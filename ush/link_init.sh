2d7af3b28985f2fc7add444d3164a3d5c7a5e130
Gang Zhao <gang.zhao@noaa.gov>
2018-09-18 16:53:01 +0000

    minor bugs in ush/link_init.sh
    Change-Id: I094f6cbde122d1ce43c5ba8cfb50e740adbb73dc

--- ush/link_init.sh
+++ ush/link_init.sh
@@ -102,8 +102,8 @@ echo
 ls -ltr $EXEC
 
 # set up the log directory for rocoto workflow running job
-WORFLOW_DIR=${TOP_RTMA}/workflow
-cd WORKFLOW_DIR
+WORKFLOW_DIR=${TOP_RTMA}/workflow
+cd $WORKFLOW_DIR
 mkdir -p logs
 mkdir -p logs/jlogfiles
 mkdir -p logs/pgmout
